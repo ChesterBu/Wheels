e1873b9c58aa028b153901b96125056a7ef770aa
chester <291164091@qq.com>
2017-12-14 22:45:21 +0800

    add scrolltotop

--- ScrollToTop/ScrollToTop.js
+++ ScrollToTop/ScrollToTop.js
@@ -1,7 +1,15 @@
 /*jshint esversion: 6 */
-(function(){
+(function() {
     let root = (typeof self == 'object' && self.self == self && self) ||
-                (typeof global == 'object' && global.global == global && global) ||
-                this || {};
-    
+        (typeof global == 'object' && global.global == global && global) ||
+        this || {};
+
+    if (typeof exports != 'undefined' && !exports.nodeType) {
+        if (typeof module != 'undefined' && !module.nodeType && module.exports) {
+            exports = module.exports = ScrollToTop;
+        }
+        exports.ScrollToTop = ScrollToTop;
+    } else {
+        root.ScrollToTop = ScrollToTop;
+    }
 }());
